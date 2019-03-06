e878029eff21850f3c8e54a87f10d07b2ab0a45f
Dylan Michael Labat <dml252@live.com>
2019-03-06 03:55:33 +0000

    Rewrite input options in detail_inquiry method

--- lib/letterboxd100/cli.rb
+++ lib/letterboxd100/cli.rb
@@ -1,4 +1,6 @@
 class Letterboxd100::CLI
+  attr_accessor :set_start
+  
   def call
     Letterboxd100::Scraper.new.create_list
     list_welcome
@@ -11,16 +13,17 @@ class Letterboxd100::CLI
   
   def start
     puts ""
-    puts "Enter the film rankings that you would like to view or 'q' to quit:"
+    puts "Enter the film rankings that you would like to view or 'quit' at any time:"
     puts "1-10, 11-20, 21-30, 31-40, 41-50, 51-60, 61-70, 71-80, 81-90, 91-100"
   
     input = gets.strip
+    @set_start = input.to_i
     
     if input == "1-10" || input == "11-20" || input == "21-30" || input == "31-40" || input == "41-50" || 
       input == "51-60" || input == "61-70" || input == "71-80" || input == "81-90" || input == "91-100"
       list_set(input)
       detail_inquiry
-    elsif input == "q"
+    elsif input == "quit"
       quit
     else
       entry_error
@@ -43,7 +46,9 @@ class Letterboxd100::CLI
     
     if input == "back"
       start
-    elsif input.to_i >= 1 && input.to_i <= 100
+    elsif input == "quit"
+      quit
+    elsif input.to_i >= @set_start && input.to_i <= (@set_start + 9)
       film_data(input.to_i)
     else
       entry_error
@@ -65,7 +70,8 @@ class Letterboxd100::CLI
     puts "Synopsis:"
     puts "#{film.synopsis}"
     puts ""
-    puts "Read more at:     #{film.url}"
+    puts "Read more at:"
+    puts "#{film.url}"
   end
   
   def entry_error
